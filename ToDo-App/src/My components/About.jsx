7848078fcbeee932c0ec7e28dcc1ada35405ce35
Arshad-khan05 <122683305+Arshad-khan05@users.noreply.github.com>
2024-01-07 12:43:28 +0530

    Added About section and some more features

--- ToDo-App/src/My components/About.jsx
+++ ToDo-App/src/My components/About.jsx
@@ -1,12 +1,77 @@
-import React from 'react'
+import React from 'react';
+
+const sectionStyle = {
+  margin: '15px 30px',
+};
 
 const About = () => {
-    return (
-        <div className='container'>
-            <h1>HI this is about section</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa asperiores minus nulla amet eum laudantium numquam accusamus cu mque optio id quaerat nobis harum fugiat, nostrum a ea nam totam. Perferendis.</p>
-        </div>
-    )
-}
+  return (
+    <>
+      <h2 style={sectionStyle}><strong>About Us</strong></h2>
 
-export default About
+      <div style={sectionStyle}>
+        <h4>Hello User</h4>
+        <p>Welcome to our Todo App, the easiest way to manage your tasks effortlessly!</p>
+      </div>
+
+      <div style={sectionStyle}>
+        <h4><u>About the Developer</u></h4>
+        <p>
+          I'm Arshad Khan, currently pursuing my Bachelor's in Technology (BTech) at Siddaganga Institute of Technology, Tumkur.
+          I developed Todo App to simplify task management and enhance your daily routine.
+        </p>
+      </div>
+
+      <div style={sectionStyle}>
+        <h4><u>Features</u></h4>
+        <ul>
+          <li>
+            <b><u>Add Tasks</u> :</b>  Just type your task and click "Add." Your tasks instantly appear on the list.
+          </li>
+          <br />
+          <li>
+            <b><u>Delete Tasks</u> :</b>  Finished a task? Hit the delete icon, and it's gone!
+          </li>
+        </ul>
+      </div>
+
+      <div style={sectionStyle}>
+        <h4><u>How to Use</u></h4>
+        <ul>
+          <li>
+            <b><u>Adding Tasks</u> :</b>  Type your task, click "Add" or press "Enter."
+          </li>
+          <br />
+          <li>
+            <b><u>Delete Tasks</u> :</b>  Click the delete icon next to a task, confirm, and it's deleted.
+          </li>
+        </ul>
+      </div>
+
+      <div style={sectionStyle}>
+        <h4><u>Why Todo App</u></h4>
+        <ul>
+          <li>
+            <b><u>Simple</u> :</b>   No complications, just a clean design.
+          </li>
+          <br />
+          <li>
+            <b><u>Quick</u> :</b>  Add or delete tasks with just a click.
+          </li>
+          <br />
+          <li>
+            <b><u>User-Friendly</u> :</b>   Easy for everyone to use
+          </li>
+        </ul>
+      </div>
+
+      <div style={sectionStyle}>
+        <h4><u>Need Help?</u></h4>
+        <p>Got questions or suggestions? Email me at <b>arshad.feedback@gmail.com</b></p>
+        <p>Thank you for choosing Todo App!</p>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- ToDo-App/src/My components/Addtodo.jsx
+++ ToDo-App/src/My components/Addtodo.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { useState } from 'react';
+
+
+const DangerButtonClicked = () => {
+    alert("Are you Sure! Do you want to clear all Todos?")
+
+    localStorage.clear();
+
+    window.location.reload();
+
+}
 const Addtodo = (props) => {
     const [title, settitle] = useState("");
     const [desc, setdesc] = useState("");
 
+
+    const DangerButton = {
+        marginLeft: '15px',
+    }
 
     const submit = (e) => {
         e.preventDefault();
@@ -16,8 +30,13 @@ const Addtodo = (props) => {
         settitle("");
         setdesc("");
     }
+    
+    const divstyle = {
+        marginLeft : '15px'
+    }
+
     return (
-        <div className='container'>
+        <div style={divstyle} className='container'>
             <h3><b>Add todo</b></h3>
             <form onSubmit={submit}>
                 <div className="mb-3">
@@ -28,7 +47,8 @@ const Addtodo = (props) => {
                     <label htmlFor="desc" className="form-label">Todo Description</label>
                     <input type="text" value={desc} onChange={(e) => setdesc(e.target.value)} className="form-control" id="exampleInputPassword1"></input>
                 </div>
-                <button type="button" className="btn btn-sm btn-success" onClick={submit}>Success</button>
+                <button type="button" className="btn btn-sm btn-success" onClick={submit}>Add</button>
+                <button style={DangerButton} id='clearbutton' type="button" onClick={DangerButtonClicked} className="btn btn-sm btn-danger" >Clear All</button>
             </form>
         </div>
     )
